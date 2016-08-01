7fe64ddcd9bc508464183d3a6e8428d58fda32d2
justin leggett <justin@codingrookie.com>
2016-08-01 17:11:04 -0500

    added img folder and images and completed task 1.

--- js/index.js
+++ js/index.js
@@ -14,10 +14,11 @@ function createCard() {
     containerDiv.style.height = "200px";
     containerDiv.style.border = "1px solid black";
     containerDiv.style.display = "inline-block";
+    containerDiv.style.textAlign = "center";
 
     // Color input #1
     var color0 = document.createElement('input');
-    color0.id = "color_" + a;
+    color0.id = "card_color_" + a;
     color0.type = "color";
     color0.setAttribute("value", "#FFFF00");
     containerDiv.appendChild(color0);
@@ -33,14 +34,34 @@ function createCard() {
     var submit = document.createElement('button');
     submit.id = "delete_" + a;
     submit.innerHTML = "Delete";
-
     containerDiv.appendChild(submit);
+
+    // random img
+    var image = document.createElement('img');
+    image.setAttribute("src", "img/blue_card.png");
+    image.style.width = "100px";
+    image.style.height = "100px";
+    containerDiv.appendChild(image);
+
+    // Sample text
+    var sampleText = document.createElement('p');
+    sampleText.innerHTML = "This is an example of sample text.";
+    containerDiv.appendChild(sampleText);
+
     existingDiv.appendChild(containerDiv);
 
 }
 
 function cardBackground() {
-    console.log("Test");
+    document.getElementById("card_color_" + a).addEventListener("input", function() {
+        this.parentNode.style.background = this.value;
+    });
+}
+
+function cardColor() {
+    document.getElementById("color_" + (a + 1)).addEventListener("input", function() {
+        this.parentNode.style.color = this.value;
+    });
 }
 
 function deleteListener() {
@@ -53,6 +74,8 @@ function deleteListener() {
 document.getElementById("create_new_card").addEventListener("click", function() {
     createCard();
     deleteListener();
+    cardBackground();
+    cardColor();
     a++;
 });
 
